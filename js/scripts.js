e6d4f8f350a18e0a339ce3fd9df15d450940f650
MasahiKon <konnonorth@gmail.com>
2023-03-18 02:41:30 -0700

    tempfolio

---
+++ js/scripts.js
@@ -0,0 +1,87 @@
+/*!
+* Start Bootstrap - Freelancer v7.0.6 (https://startbootstrap.com/theme/freelancer)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    const contactForm = document.querySelector("#contactForm");
+    const userName = document.querySelector("#name");
+    const userEmail = document.querySelector("#email");
+    const formMessage = document.querySelector("#message");
+    const formSubmit = document.querySelector("#submitButton");
+    const submitButton = document.querySelector("#submitButton");
+
+    emailjs.init('aYZSl6EbX56GwP8tD');
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 72,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    contactForm.addEventListener("submit", function(event) {
+
+        event.preventDefault();
+
+        formSubmit.disabled = true;
+
+        this.contact_number.value = Math.random() * 100000 | 0;
+
+        emailjs.sendForm('service_qnbzdph', 'template_vynvtxh', this)
+            .then(function() {
+                clearForm();
+                submitButton.textContent = "Sent!"
+            })
+            .catch(function() {
+                clearForm();
+                submitButton.textContent = "Falied"
+            });
+
+        function clearForm() {
+            userName.value = "";
+            userEmail.value = "";
+            formMessage.value = "";
+        }
+    });
+});
